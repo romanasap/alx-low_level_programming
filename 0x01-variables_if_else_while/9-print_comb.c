9633fbee8cc0649afe8c8784ae1992fa68355998
romanasap <abenezere@wujo.app>
2022-04-10 07:33:07 -0700

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,15 +9,15 @@ int main(void)
 {
 int c = 0;
 
-while (c < 10)
+for (c = 48 ; c <= 57 ; c++)
 {
-putchar(48 + c);
-if (c != 9)
+putchar(c);
+if (c == 57)
 {
+break;
+}
 putchar(',');
 putchar(' ');
-}
-c++;
 }
 putchar('\n');
 return (0);
