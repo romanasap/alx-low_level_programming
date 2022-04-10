17f084bdc9ef246bb7f4ddd3a80555b97b44821a
romanasap <abenezere@wujo.app>
2022-04-10 07:39:00 -0700

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,7 +3,7 @@
  * main - main block
  * Description: prints all single digit numbers of base 10
  * starting from 0, following by a new line
- * Return: 0 
+ * Return: Always 0
  */
 int main(void)
 {
